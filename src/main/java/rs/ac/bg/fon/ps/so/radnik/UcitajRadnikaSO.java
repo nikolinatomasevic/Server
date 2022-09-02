7f6f828ea464cf3a3ac9d52faacb178d93753b38
nikolinatomasevic <nikolina.tomasevic2000@gmail.com>
2022-09-02 15:44:10 +0200

    Dodata validacija broja telefona koriscenjem numlookupapi web servisa

--- src/main/java/rs/ac/bg/fon/ps/so/radnik/UcitajRadnikaSO.java
+++ src/main/java/rs/ac/bg/fon/ps/so/radnik/UcitajRadnikaSO.java
@@ -5,6 +5,14 @@
  */
 package rs.ac.bg.fon.ps.so.radnik;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
 import rs.ac.bg.fon.ps.domain.Radnik;
 import rs.ac.bg.fon.ps.so.AbstractSO;
 
@@ -26,6 +34,29 @@ public class UcitajRadnikaSO extends AbstractSO {
 		}
 		if (radnik.getPrezime().isEmpty()) {
 			message += "Polje 'prezime' radnika ne sme biti prazno!\n";
+		}
+		if (!radnik.getBrojTelefona().isEmpty()) {
+			String brojTelefona = "";
+			if (radnik.getBrojTelefona().startsWith("+")) {
+				brojTelefona = radnik.getBrojTelefona();
+			} else {
+				brojTelefona = "+381" + radnik.getBrojTelefona().substring(1);
+			}
+			String apikey = "lwMAnEI9xBpMp5pyL3fjzGvoDw6TVfSnNWuEdEDn";
+
+			URL url = new URL("https://api.numlookupapi.com/v1/validate/" + brojTelefona + "?apikey=" + apikey);
+
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+
+			Gson gson = new Gson();
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+			JsonObject rezultat = gson.fromJson(br, JsonObject.class);
+			if (!rezultat.get("valid").getAsBoolean() || !rezultat.get("country_code").getAsString().equals("RS")) {
+				message += "Polje 'broj telefona' radnika mora biti broj domace mreze!\n";
+			}
 		}
 		if (radnik.getMejl().isEmpty()) {
 			message += "Polje 'mejl' radnika ne sme biti prazno!\n";
